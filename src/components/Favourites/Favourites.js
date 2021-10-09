390e654dddf07028d5d80e7993bb7bdaf10c849d
Jaroslav-K <jaroslavkopcan@outlook.sk>
2021-10-09 23:59:47 +0200

    text change

--- src/components/Favourites/Favourites.js
+++ src/components/Favourites/Favourites.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import classes from './Favourites.module.css'
-
+import React from "react"
+import classes from "./Favourites.module.css"
 
 const AddFavourite = () => {
-    return (
-        <>
-           <span className={classes.addToFavourites}></span> 
-           <i class="fas fa-star">Add to Favourites	&#11088;</i>
-        </>
-    )
+  return (
+    <>
+      <span className={classes.addToFavourites}></span>
+      <i class="fas fa-star">I am into it. &#11088;</i>
+    </>
+  )
 }
 
 export default AddFavourite;
