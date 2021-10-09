29fa9bee24ed5f72b52ff9b456b4f67c2d57b2d7
Jaroslav-K <jaroslavkopcan@outlook.sk>
2021-10-10 00:01:19 +0200

    Remove favorites & saving it to local

--- src/App.js
+++ src/App.js
@@ -1,61 +1,96 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
+import { Route, Switch } from "react-router-dom"
 
-import './App.css';
-import Header from './components/Header/Header';
-import MovieSearch from './components/MovieSearch/MovieSearch';
-import SearchBox from './components/SearchBox/SearchBox';
-import AddFavourite from './components/Favourites/Favourites';
+
+import Header from "./components/Header/Header"
+import MovieSearch from "./components/MovieSearch/MovieSearch"
+import SearchBox from "./components/SearchBox/SearchBox"
+import AddFavourite from "./components/Favourites/Favourites"
+import FavouritesPage from "./pages/FavouritesPage"
+import RemoveFavourites from "./components/Favourites/RemoveFavourites"
+
+import "./App.css"
 
 /*import classes from './MovieSearch.module.css'*/
 
-
 const App = () => {
-	const [movies, setMovies] = useState([]);
-  const [favourites, setFavourites] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [movies, setMovies] = useState([])
+  const [favourites, setFavourites] = useState([])
+  const [searchValue, setSearchValue] = useState("")
 
   const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9eb462ba`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9eb462ba`
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
+    const response = await fetch(url)
+    const responseJson = await response.json()
 
     if (responseJson.Search) {
-      setMovies(responseJson.Search);
+      setMovies(responseJson.Search)
     }
-  };
+  }
 
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
-  const addFavouriteMovie = (movie) => {
-    const newFavouriteList = [...favourites, movie];
-    setFavourites(newFavouriteList);
-  };
-    
-  return (
-    <div>
-      <div>
-        <Header heading="Blockbusters"/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-      </div>
-      <div>
-        <MovieSearch movies= {movies} handleFavouritesClick={addFavouriteMovie} favouriteComponent={AddFavourite} />
-      </div>
-      <div>
-        <Header heading="Favourites"/>
-      </div>
-      <div>
-        <MovieSearch 
-            movies= {favourites} 
-            handleFavouritesClick={addFavouriteMovie} 
-            favouriteComponent={AddFavourite} 
-          />
-      </div>
-    </div>  
-    
-  );
-};
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites')
+    );
+    setFavourites(movieFavourites);
+  }, []);
 
-export default App;
+  const saveToBrowserStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  };
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie]
+    setFavourites(newFavouriteList);
+    saveToBrowserStorage(newFavouriteList);
+  };
+  
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourites(newFavouriteList);
+    saveToBrowserStorage(newFavouriteList);
+  };
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        {/* <div> */}
+        <div>
+          <Header heading="Blockbusters" />
+          <SearchBox
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+        </div>
+        <div>
+          <MovieSearch
+            movies={movies}
+            handleFavouritesClick={addFavouriteMovie}
+            favouriteComponent={AddFavourite}
+          />
+        </div>
+      </Route>
+      <Route path="/favourites">
+        <div>
+          <Header heading="Favourites" />
+        </div>
+        <div>
+          <FavouritesPage
+            favourites={favourites}
+            addFavouriteMovie={removeFavouriteMovie}
+            AddFavourite={RemoveFavourites}  
+          />
+        </div>
+      </Route>
+      {/* </div>  */}
+    </Switch>
+  )
+}
+
+export default App
---
+++ src/components/Favourites/RemoveFavourites.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const RemoveFavourites = () => {
+  return (
+    <>
+      <span></span>
+      <i class="fas fa-star">Naah, don't like it anymore. &#10006;</i>
+    </>
+  )
+}
+
+export default RemoveFavourites;
